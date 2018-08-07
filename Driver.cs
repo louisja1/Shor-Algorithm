0ef3d75b2421023b091763bc3473ae208703cdcd
louisja1 <674808516@qq.com>
2018-08-07 11:53:20 +0800

    Initialize

---
+++ Driver.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Shor
+{
+    class Driver
+    {
+        static long gcd(long a, long b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the number to be factored...");
+            long N = Convert.ToInt64(Console.ReadLine());
+            
+            long p = factor(N);
+            Console.WriteLine("Success!");
+            Console.WriteLine("-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-");
+            Console.WriteLine($"{N} = {p} * {N / p}");
+            Console.ReadKey();
+        }
+
+        static long[] continuedFractionExtension(double x)
+        {
+            double PRECISION = 1e-9;
+            int ITERATION = 100;
+            long[] fractions;
+            fractions = new long[100];
+            long integer = 0;
+            double frac = 0;
+            double currentNum = x;
+            int i = 0;
+            for(int ii = 0; ii < 100; ++ii)
+            {
+                fractions[ii] = -1;
+            }
+            while (true)
+            {
+                integer = (long)currentNum;
+                fractions[i++] = integer;
+                frac = currentNum - integer;
+                if (frac < PRECISION || i >= ITERATION)
+                {
+                    break;
+                }
+                currentNum = 1 / frac;
+            }
+            return fractions;
+        }
+
+        static Frac[] restoreContinuedFraction(long[] arr)
+        {
+            int i = 0;
+            Frac[] fracArray = new Frac[100];
+            while (true)
+            {
+                if (arr[i] == -1)
+                {
+                    fracArray[i++] = new Frac(-1, 1);
+                    if(i == 100)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+                Frac cur = new Frac(0, 1);
+                for (int j = i; j >= 0; --j)
+                {
+                    cur = cur.add(new Frac(arr[j], 1));
+                    if (j == 0)
+                    {
+                        fracArray[i++] = cur;
+                    }
+                    else
+                    {
+                        cur = cur.inverse();
+                    }
+                }
+            }
+            return fracArray;
+        }
+
+        static long factor(long N)
+        {
+            while (true)
+            {
+                if (N % 2 == 0)
+                {
+                    return 2;
+                }
+                Random ran = new Random();
+                int x = ran.Next(1, (int)(N - 1));
+                Console.WriteLine($"generate random number: {x}");
+                long g = gcd(x, N);
+                if (g != 1 && g != N)
+                {
+                    return g;
+                }
+                long r = qQrderFinding(x, N);
+                if (r == 0 || r == -1)
+                {
+                    Console.WriteLine("Fail...");
+                    continue;
+                }
+                if (r % 2 == 0 && modCondition(x, r, N))
+                {
+                    long g1 = gcd(quickPower(x, r / 2, N) - 1, N);
+                    if (g1 != 1 && g1 != N)
+                    {
+                        return g1;
+                    }
+                    long g2 = gcd(quickPower(x, r / 2, N) + 1, N);
+                    if (g2 != 1 && g2 != N)
+                    {
+                        return g2;
+                    }
+                }
+                Console.WriteLine("Fail...");
+            }
+        }
+
+        static bool modCondition(long x, long r, long N)
+        {
+            return (quickPower(x, r / 2, N) + N) % N != N - 1;
+        }
+
+        static long quickPower(long x, long y, long N)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            long tmp = quickPower(x, y / 2, N);
+            long ans = (tmp * tmp) % N;
+            if (y % 2 == 1)
+            {
+                ans = (ans * x) % N;
+            }
+            return ans;
+        }
+
+        static long qQrderFinding(long x, long N)
+        {
+            double sr = getPhaseEstimation(x, N);
+            if(sr == 0)
+            {
+                return -1;
+            }
+            //Console.WriteLine($"sr: {sr}");
+            long[] arr = continuedFractionExtension(sr);
+            Frac[] fracArray = restoreContinuedFraction(arr);
+            foreach (Frac item in fracArray)
+            {
+                long r = item.dominator;
+                if (quickPower(x, r, N) % N == 1)
+                {
+                    return r;
+                }
+            }
+            return 0;
+        }
+
+        static double getPhaseEstimation(long a, long N)
+        {
+            // Console.WriteLine($"getPhaseEstimation: the random number：{a}, N: {N}");
+            double sr = 0;
+            double dom = 1.0;
+            int TBIT = 7;
+            using (var sim = new QuantumSimulator())
+            {
+                var res = quantumOrderFinding.Run(sim, a, N).Result;
+                for (int i = 0; i < TBIT; i++)
+                {
+                    dom *= 2.0;
+                    if (res[i] == 1)
+                    {
+                        sr = sr * 2 + 1;
+                    }
+                    else
+                    {
+                        sr *= 2;
+                    }
+                }
+                sr /= dom;
+            }
+            return sr;
+        }
+
+        class Frac
+        {
+            public long numerator, dominator;
+            public Frac()
+            {
+                numerator = 0;
+                dominator = 1;
+            }
+            public Frac(long _numerator, long _dominator)
+            {
+                numerator = _numerator;
+                dominator = _dominator;
+            }
+
+            public Frac add(Frac other)
+            {
+                long newDom = this.dominator * other.dominator;
+                long newNum = this.numerator * other.dominator + other.numerator * this.dominator;
+                Frac ret = new Frac(newNum, newDom);
+                return ret.reduce();
+            }
+            public Frac reduce()
+            {
+                long d = gcd(numerator, dominator);
+                numerator /= d;
+                dominator /= d;
+                return this;
+            }
+            public long gcd(long a, long b)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+                return gcd(b, a % b);
+            }
+
+            public void print()
+            {
+                Console.WriteLine("{0}/{1}", numerator, dominator);
+            }
+
+            public Frac inverse()
+            {
+                return new Frac(dominator, numerator);
+            }
+        }
+    }
+}
---
+++ Shor.qs
@@ -0,0 +1,131 @@
+﻿namespace Shor
+{
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Primitive;
+	open Microsoft.Quantum.Extensions.Math;
+
+    operation U_xN(qubits : Qubit[], constant : Int, N : Int) : () {
+        body {
+            ModularMultiplyByConstantLE(constant, N, LittleEndian(qubits));
+        }
+        controlled auto;
+    }
+
+    function quickPower(x : Int, y : Int) : (Int) {
+        mutable tmp = 1;
+        mutable ans = 1;
+        mutable yy = y;
+        for (i in 0 .. 20) {
+            if (yy % 2 == 1) {
+                set ans = ans * tmp;
+            }
+            set yy = yy / 2;
+            if (yy == 0) {
+                return ans;
+            }
+            set tmp = tmp * x;
+        }
+        return ans;
+    }
+
+    function quickPowerWithModule(x : Int, y : Int, N : Int) : (Int) {
+        mutable tmp = 1;
+        mutable ans = 1;
+        mutable yy = y;
+        for (i in 0 .. 20) {
+            if (yy % 2 == 1) {
+                set ans = (ans * tmp) % N;
+            }
+            set yy = yy / 2;
+            if (yy == 0) {
+                return ans;
+            }
+            set tmp = (tmp * x) % N;
+        }
+        return ans;
+    }
+
+
+    operation measure(qubits : Qubit[], t : Int) : (Int[])
+    {
+        body {
+            mutable measurement = new Int[t];
+            for (i in 0 .. t - 1) {
+                if (M(qubits[i]) == One) {
+                    set measurement[i] = 1;
+                } else {
+                    set measurement[i] = 0;
+                }
+            }
+            return measurement;
+        }
+    }
+
+    operation quantumFourierTransform(qubits : Qubit[]) : () {
+        body {
+			let pi = PI();
+            let n = Length(qubits);
+            mutable powers = new Double[n + 5];
+            set powers[0] = 1.0;
+            for (i in 1 .. n) {
+                set powers[i] = powers[i - 1] * 2.0;
+            }
+
+            for (i in 0 .. n - 1) {
+                H(qubits[i]);
+                for (j in i + 1 .. n - 1) {
+                    (Controlled R1) ([qubits[j]], (2.0 * pi / powers[j - i + 1], qubits[i]));
+                }
+            }
+            for (i in 0 .. n / 2 - 1) {
+                SWAP(qubits[i], qubits[n - 1 - i]);
+            }
+        }
+        adjoint {
+			let pi = PI();
+            let n = Length(qubits);
+            mutable powers = new Double[n + 5];
+            set powers[0] = 1.0;
+            for (i in 1 .. n) {
+                set powers[i] = powers[i - 1] * 2.0;
+            }
+
+            for (i in 0 .. n / 2 - 1) {
+                SWAP(qubits[i], qubits[n - 1 - i]);
+            }
+            for (p in 0 .. n - 1) {
+                let i = n - 1 - p;
+                for (q in 0 .. p - 1) {
+                    let j = n - 1 - q; 
+                    (Controlled R1) ([qubits[j]], (-2.0 * pi / powers[j - i + 1], qubits[i]));
+                }
+                H(qubits[i]);
+            }
+        }
+    }
+
+    operation quantumOrderFinding(a : Int, N : Int) : (Int [])
+    {
+        body {
+            let l = 5;
+            let t = 7;
+            mutable result = new Int[t];
+            using (qs = Qubit[t + l]) {
+                let x = qs[0 .. t - 1];
+                let y = qs[t .. t + l - 1];
+                for (i in 0 .. t - 1) {
+                    H(x[i]);
+                }
+                X(y[0]);
+                for (i in 0 .. t - 1) {
+                    let r = t - 1 - i;
+                    (Controlled U_xN) ([x[i]], (y, quickPowerWithModule(a, quickPower(2, r), N), N));
+                }
+                (Adjoint quantumFourierTransform) (x);
+                set result = measure(x, t);
+                ResetAll(qs);
+            }	
+			return result;
+        }
+    }
+}
